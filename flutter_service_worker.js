d10933befcf636ce3b83009f3ed9852b906cb71e
Nguyen Nguyen <nguyenny@sendo.com>
2021-03-27 00:02:30 +0700

    update web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "7ee8d0621734fef97acad84c8736be6d",
-"index.html": "3e0737ce91fe82f2bf8700008cd001d5",
-"/": "3e0737ce91fe82f2bf8700008cd001d5",
-"main.dart.js": "d4f0a85eb0aef9b99cc71b18a91efdd1",
+"index.html": "1268ebc58346024b085625ceb35beece",
+"/": "1268ebc58346024b085625ceb35beece",
+"main.dart.js": "d20f2489d5d0d1405bd67271967833dc",
 "favicon.png": "03150e17e6215bd1a25d5f5cb0ec463f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
